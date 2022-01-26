6bd00e0c584c849a4515dcfde93eb33def71327d
acferM <promacfer@gmail.com>
2022-01-26 14:26:33 -0300

    static info done

---
+++ src/pages/index.tsx
@@ -0,0 +1,48 @@
+import type { NextPage } from 'next'
+import { AiOutlineBehance, AiFillInstagram } from 'react-icons/ai'
+import { FaLinkedinIn } from 'react-icons/fa'
+
+import styles from './home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+		<div className={styles.container}>
+			<img src="logo.svg" alt="Dahora" />
+			<img src="me.png" alt="Carlos Eduardo"/>
+			
+			<p>@Cadudahora :)</p>
+			
+			<main className={styles.tree} >
+				<button>
+					CONFIRA <strong>MEU SITE</strong>
+				</button>
+
+				<button>
+					CONFIRA <strong>MEU BEHANCE</strong>
+				</button>
+
+				<button>
+					MEU PORTFÓLIO
+				</button>
+				
+				<button>
+					MEU PORTFÓLIO
+				</button>
+			</main>
+
+			<footer className={styles.footer}>
+				<a href="https://behance.net/dahora">
+					<AiOutlineBehance size={36} color="#fff" />
+				</a>
+				<a href="https://instagram.com/caxdu">
+					<AiFillInstagram size={36} />
+				</a>
+				<a href="https://linkedin.com/in/dahora">
+					<FaLinkedinIn size={36} color="#fff" />
+				</a>
+			</footer>
+		</div>
+	)
+}
+
+export default Home
